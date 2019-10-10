27baf5dc52135227d5dc9cb60c0c26160cce54b1
Eric Almadova <EricAlmadovawow@gmail.com>
2019-10-09 22:40:30 -0700

    Initial commit of potential front end

---
+++ CsgoViewer/CsgoViewer/CsgoViewerMainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CsgoViewer
+{
+    /// <summary>
+    /// Interaction logic for CsgoViewerMainWindow.xaml
+    /// </summary>
+    public partial class CsgoViewerMainWindow : Window
+    {
+        public CsgoViewerMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Sup");
+        }
+    }
+}
